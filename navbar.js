b2a350e352719a7a79857d21c016531f22b5ca69
Joona <joonatan.karhu@outlook.com>
2022-04-01 04:19:38 +0300

    update

--- navbar.js
+++ navbar.js
@@ -3,8 +3,12 @@ const links = document.querySelector(".links");
 const navPosition = document.querySelector(".nav-toggle");
 const navIcon = document.querySelector(".mobile-nav-icon");
 
+
+
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
   navIcon.classList.toggle("black-nav-btn");
   navPosition.classList.toggle("nav-btn-position");
+
 });
+
