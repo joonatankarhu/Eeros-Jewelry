79f01fb3f7b9ef3a75eb22cd8300e39427ce5d23
Joona <joonatan.karhu@outlook.com>
2022-04-16 21:10:47 +0300

    s

--- ImgCarosel.js
+++ ImgCarosel.js
@@ -5,12 +5,11 @@ var images = new Array(
     "./assets/images/products/header-image.jpg",
     "./assets/images/products/green_stone_amulet.jpg",
     );
-var i = setInterval(auto, 2500);
+var i = setInterval(auto, 1800);
 
-function auto()
-  {
+function auto() {
     x++;
     if (x == images.length)
        x=0;
     document.getElementById('bigImage').src=images[x];      
-  }
+}
